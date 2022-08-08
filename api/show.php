1760a708a1f90d7f49658ceebe8d7ef1502c872d
yizhen <yiyi07221014@gmail.com>
2022-08-08 16:23:29 +0800

    新增上課內容

---
+++ api/show.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once "../base.php";
+
+$row=$Movie->find($_POST['id']);
+
+// if($row['sh']==1){
+//     $row['sh']=0;
+// }else{
+//     $row['sh']=1;
+// }
+
+// 縮減
+// $row['sh']=($row['sh']==1)?0:1;
+
+// 縮減
+$row['sh']=($row['sh']+1)%2;
+$Movie->save($row);
